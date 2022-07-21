fb5d58b841f8c8154161cbd86fe05fffe627eb6a
Kristi Liu <ksliu@ucsb.edu>
2022-07-21 11:17:17 -0700

    staging to show automerge error

--- user-input/user-input.sh
+++ user-input/user-input.sh
@@ -4,3 +4,6 @@ echo "Hello $name."
 # The 'read' command requests user input. 
 # We use '$' to recall the variable established by 'read'
 # Adding a line for Amanda
+
+
+# Kristi adding a line to show automerge 
