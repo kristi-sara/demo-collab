539df9067d14c8097404b67d60e8207bd3390334
Kristi Liu <ksliu@ucsb.edu>
2022-10-19 12:04:16 -0700

    reset user input file for collab conflict

--- user-input/user-input.sh
+++ user-input/user-input.sh
@@ -4,6 +4,5 @@ echo "Hello $name!"
 # The 'read' command requests user input. 
 # We use '$' to recall the variable established by 'read'
 
-# Adding a line for Amanda
-# Kristi adding a line to show automerge 
+
 
